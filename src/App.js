3521eab5c7dc1d514ebd95bf86b18870ca14c942
ttcuong307 <ttcuong307@gmail.com>
2021-04-14 23:04:02 +0700

    20210414

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
